4d30bef9c0664fcefa7de75ff98d548ffe017ef9
sodex4real11 <sodex4real11@gmail.com>
2024-05-02 21:15:43 -0700

    python variables

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 12's module.
+'''
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''Creates multiple copies of items in a tuple.
+    '''
