da1b1f1d10d27fee6127db26d8cb841e4e12a03e
sodex4real11 <sodex4real11@gmail.com>
2025-10-01 03:35:32 +0700

    update

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenREfreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,6 @@ urlpatterns = [
     # The checker specifically mentioned the path 'api' in the instructions.
     # Let's use 'api/' as the main prefix.
     path('api/', include('chats.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
