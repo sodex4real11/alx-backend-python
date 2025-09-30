20a81121cd88ebd2694bc07fe8b2c290fa87b7e2
sodex4real11 <sodex4real11@gmail.com>
2025-10-01 03:00:55 +0700

    updated

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,17 +1,91 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import viewsets, filters, status
 from rest_framework.response import Response
-from rest_framework import permissions
-from .models import Message
-from .serializers import MessageSerializer
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
+# -----------------------------
+# Conversation ViewSet
+# -----------------------------
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['title']  # search by conversation title
+    search_fields = ['title']
 
+    # List all conversations
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Retrieve a single conversation
+    def retrieve(self, request, *args, **kwargs):
+        conversation = self.get_object()
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Create a new conversation
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # Update a conversation
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Delete a conversation
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# -----------------------------
+# Message ViewSet
+# -----------------------------
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['content', 'sender']  # search by content or sender
+    search_fields = ['content', 'sender']
+
+    # List all messages
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Retrieve a single message
+    def retrieve(self, request, *args, **kwargs):
+        message = self.get_object()
+        serializer = self.get_serializer(message)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Create a new message
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # Update a message
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Delete a message
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
