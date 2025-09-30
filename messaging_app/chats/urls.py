a27e6849b97809aaaa90929e72226db29573ebd0
sodex4real11 <sodex4real11@gmail.com>
2025-10-01 03:34:43 +0700

    update

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -5,6 +5,7 @@ from .views import ConversationViewSet, MessageViewSet
 # The main router for the top-level resource (Conversations)
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet)
 
 # Create a nested router for Messages, registered under Conversations.
 # The `lookup='conversation'` tells the router that the URL parameter for the
@@ -15,4 +16,5 @@ conversations_router.register(r'messages', MessageViewSet, basename='conversatio
 # The API URLs are now nested.
 urlpatterns = [
     path('messages/', views.messages_list, name='messages-list'),  # example route
+    path('', include(router.urls)
 ]
